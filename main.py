48d9850bd9d7056d95a305f41414dc0ca90a1f10
Aditya Anand <adianand2002@gmail.com>
2023-12-19 01:01:07 +0530

    send the mlflow metrics to dagshub

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
-
 import os
 import warnings
 import sys
@@ -15,7 +11,7 @@ from urllib.parse import urlparse
 import mlflow
 from mlflow.models.signature import infer_signature
 import mlflow.sklearn
-
+import mlflow.pytorch
 import logging
 
 logging.basicConfig(level=logging.WARN)
@@ -77,16 +73,9 @@ if __name__ == "__main__":
         mlflow.log_metric("mae", mae)
 
         
-        # # For remote server only (Dagshub)
-        # remote_server_uri = "https://dagshub.com/entbappy/MLflow-Basic-Demo.mlflow"
-        # mlflow.set_tracking_uri(remote_server_uri)
-
-
-        # For remote server only (AWS)
-        # remote_server_uri = "http://ec2-54-147-36-34.compute-1.amazonaws.com:5000/"
-        # mlflow.set_tracking_uri(remote_server_uri)
-
-
+        # For remote server only (Dagshub)
+        remote_server_uri = "https://dagshub.com/xxxxxx/xxxx.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
